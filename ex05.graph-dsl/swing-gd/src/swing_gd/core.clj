a5d1d7c8930bc0ee6b5e3f0d498faf6a7c5b948f
anton0xf <anton0xf@gmail.com>
2023-03-14 02:52:22 +0300

    05.sgd: draw something on swing panel
    Signed-off-by: anton0xf <anton0xf@gmail.com>

--- ex05.graph-dsl/swing-gd/src/swing_gd/core.clj
+++ ex05.graph-dsl/swing-gd/src/swing_gd/core.clj
@@ -1,7 +1,32 @@
 (ns swing-gd.core
-  (:gen-class))
+  (:gen-class)
+  (:import [javax.swing JFrame JPanel]
+           [java.awt Color Graphics Graphics2D]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn render [#^Graphics g w h]
+  (doto g
+    (.setColor (Color/GREEN))
+    (.drawLine 0 0 w h)
+    (.drawLine w 0 0 h)))
+
+(defn create-panel []
+  "Create a panel with a customised render"
+  (proxy [JPanel] []
+    (paintComponent [g]
+      (proxy-super paintComponent g)
+      (render g (. this getWidth) (. this getHeight)))))
+
+(defn run []
+  (let [panel (create-panel)
+        frame (JFrame. "graphic dsl")]
+    (doto frame
+      (.add panel)
+      (.setSize 640 400)
+      (.setVisible true))))
+
+(defn -main [& args]
+  (run))
+
+(comment
+  (run)
+  )
